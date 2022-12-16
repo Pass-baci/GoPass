e2859cd0d4acf12327a5b4ad371daa5721d9c48e
v_chgvchen <v_chgvchen@tencent.com>
2022-12-16 17:55:27 +0800

    add test

---
+++ 01micro/test/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Pass-baci/common"
+	"github.com/asim/go-micro/v3"
+	"github.com/asim/go-micro/v3/registry"
+	"github.com/go-micro/plugins/v3/registry/consul"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"test/handler"
+	pb "test/proto"
+
+	"github.com/micro/micro/v3/service/logger"
+)
+
+func main() {
+	// 配置consul
+	consulClient := consul.NewRegistry(func(options *registry.Options) {
+		options.Addrs = []string{
+			"localhost:8500",
+		}
+	})
+
+	// 配置中心consul
+	conf, err := common.GetConsulConfig("localhost", 8500, "/micro/config")
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	// 使用配置中心获取数据库配置
+	var mysqlConfig = &common.MysqlConfig{}
+	if mysqlConfig, err = common.GetMysqlFromConsul(conf, "mysql"); err != nil {
+		logger.Fatal(err)
+	}
+
+	// 连接数据库
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		mysqlConfig.User, mysqlConfig.Pwd, mysqlConfig.Host, mysqlConfig.Port, mysqlConfig.Database)
+	if _, err = gorm.Open(mysql.Open(dsn), &gorm.Config{}); err != nil {
+		logger.Fatal(err)
+	}
+
+	// Create service
+	srv := micro.NewService(
+		micro.Name("test"),
+		micro.Version("1.0"),
+		micro.Registry(consulClient),
+	)
+
+	// Register handler
+	pb.RegisterTestHandler(srv.Server(), handler.New())
+
+	// Run service
+	if err = srv.Run(); err != nil {
+		logger.Fatal(err)
+	}
+}
